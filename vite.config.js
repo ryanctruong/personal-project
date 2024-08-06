e6b4da62b2b0a35e4d15e3229ca5b02712f3ec74
Ryan Truong <ryanctruong03@gmail.com>
2024-08-05 22:47:24 -0500

    change username

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal-project/",
-  plugins: [react()],
-})
+  base: '/personal-project/',
+  plugins: [react()]
+});
