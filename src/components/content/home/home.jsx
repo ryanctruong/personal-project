f12c1bb10e18130678e322d3f54222f5a9f0c6d1
Ryan Truong <ryanctruong03@gmail.com>
2024-08-07 23:12:57 -0500

    finalized profile-card

--- src/components/content/home/home.jsx
+++ src/components/content/home/home.jsx
@@ -1,4 +1,5 @@
 import './home.css'
+import ProfilePic from '/images/dog-profile-pic.png'
 import Github from '/images/github.png'
 import Linkedin from '/images/linkedin.png'
 import Gmail from '/images/gmail.png'
@@ -6,13 +7,13 @@ import Gmail from '/images/gmail.png'
 const Home = () => {
     return (
         <div className='home-main-box'>
-            <div className='profile-title'>
-                <div className='picture common-styles'>
-                    <div className='profile-picture'>
-                        <img src={''} id='profile-pic'></img>
+            <div className='profile-card'>
+                <div className='picture'>
+                    <div className='picture-container'>
+                        <img src={ProfilePic} id='profile-pic'></img>
                     </div>
                 </div>
-                <div className='basic-desc common-styles'>
+                <div className='basic-desc'>
                     <ul>
                         <li id='full-name'>Ryan Truong</li>
                         <li id='job-title'>Software Engineer</li>
@@ -20,31 +21,32 @@ const Home = () => {
                         <li id='university'><a href="https://www.belmont.edu/" target='__blank'>Belmont University</a></li>
                     </ul>
                 </div>
-                <div className='links common-styles'>
+                <div className='social-media'>
                     <ul>
                         <li className="li-github">
                             <div className="sti_container">
-                                <button className="btn">
-                                    <span className="btn-icon"><img src={Github} /></span>
-                                    <span className="btn-text">Github</span>
+                                <button className="social-media-btn" onClick={() => window.open("https://github.com/ryanctruong", "_blank", "noopener,noreferrer")}>
+                                    <span className="social-media-icon"><img src={Github} alt="Github Icon" /></span>
+                                    <span className="social-media-text">Github</span>
                                 </button>
                             </div>
                         </li>
                         <li className="li-linkedin">
                             <div className="sti_container">
-                                <button className="btn">
-                                    <span className="btn-icon"><img src={Linkedin} /></span>
-                                    <span className="btn-text">Linkedin</span>
+                                <button className="social-media-btn" onClick={() => window.open("https://www.linkedin.com/in/ryanctruong/", "_blank", "noopener,noreferrer")}>
+                                    <span className="social-media-icon"><img src={Linkedin} /></span>
+                                    <span className="social-media-text">Linkedin</span>
                                 </button>
                             </div>
                         </li>
                         <li className="li-gmail">
                             <div className="sti_container">
-                                <button className="btn">
-                                    <span className="btn-icon"><img src={Gmail} /></span>
-                                    <span className="btn-text">Gmail</span>
+                                <button className="social-media-btn" onClick={() => window.location.href = "mailto:rtruong.cs@gmail.com"}>
+                                    <span className="social-media-icon"><img src={Gmail} alt="Gmail Icon" /></span>
+                                    <span className="social-media-text">Gmail</span>
                                 </button>
                             </div>
+
                         </li>
                     </ul>
                 </div>
