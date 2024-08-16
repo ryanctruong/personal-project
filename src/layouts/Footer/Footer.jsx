707faa12ae9e63aa8af0606add2e2125213b1017
Ryan Truong <ryanctruong03@gmail.com>
2024-08-15 23:38:01 -0500

    test

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div className='white-bar'>
+            <p>&copy; 2024 Created by Ryan Truong. All rights reserved.</p>
+        </div>
+    )
+}
+
+export default Footer;
