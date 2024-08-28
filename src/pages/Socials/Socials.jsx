965c0ff5f19d5e24f4f923dab2f4c080a7d930b4
Ryan Truong <ryanctruong03@gmail.com>
2024-08-27 22:05:30 -0500

    move data to utils

--- src/pages/Socials/Socials.jsx
+++ src/pages/Socials/Socials.jsx
@@ -1,16 +1,8 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
+import { SOCIALMEDIA } from '../../utils/SocialMediaData,jsx';
 import './styles/socials.css';
 import './styles/socials-list.css';
-
-import linkedin from '/images/sm/linkedin-bh.svg';
-import linkedinTongue from '/images/sm/linkedin-tongue.svg'
-import github from '/images/sm/github-bh.svg';
-import githubTongue from '/images/sm/github-tongue.svg'
-import gmail from '/images/sm/gmail-bh.svg';
-import gmailTongue from '/images/sm/gmail-tongue.svg'
-import cv from '/images/sm/cv-bh.svg';
-import cvTongue from '/images/sm/cv-tongue.svg'
 
 const Socials = ({ displayType, colors }) => {
     const [hoveredImage, setHoveredImage] = useState(null);
@@ -38,31 +30,7 @@ const Socials = ({ displayType, colors }) => {
     return (
         <div className={`socials-main-box`}>
             <div className='socials-list'>
-                {[{
-                    src: linkedin,
-                    onHov: linkedinTongue,
-                    href: "https://www.linkedin.com/in/ryanctruong/",
-                    label: "Linkedin",
-                    description: "Connect with me!"
-                }, {
-                    src: github,
-                    onHov: githubTongue,
-                    href: "https://github.com/ryanctruong",
-                    label: "Github",
-                    description: "Check out my code!"
-                }, {
-                    src: gmail,
-                    onHov: gmailTongue,
-                    href: "mailto:rtruong.cs@gmail.com",
-                    label: "Gmail",
-                    description: "Contact Me!"
-                }, {
-                    src: cv,
-                    onHov: cvTongue,
-                    href: "",
-                    label: "CV",
-                    description: "Last Updated: 09/01/2024"
-                }].map((item, index) => (
+                {SOCIALMEDIA.map((item, index) => (
                     <motion.div
                         className={`social-media ${displayType ? 'light' : 'dark'}`}
                         style={{ boxShadow: `0 0 0.5em ${pokeTheme_ONE}` }}
